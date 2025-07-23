c5273fe8c1a529d85938f3008d105d57867b5afd
Peter Tso <tsopeter@ku.edu>
2025-07-23 17:38:06 -0500

    Changes

--- Comms/client.cpp
+++ Comms/client.cpp
@@ -4,7 +4,11 @@
 
 int main () {
     const char str[] = "Hello, World!";
-    s3_IP_Client client("192.168.193.20", 9000);
+    std::string ip = "192.168.193.20";
+    s3_IP_Client client(ip, 9000);
+    if (!client.connect()) {
+        std::cerr << "Unable to connect to: " << ip << '\n';
+    }
     client.Transmit((void*)str, strlen(str));
     client.disconnect();
 }
