fc3b24f678fdfd28197f276e439ebc1b2562efd0
Peter Tso <tsopeter@ku.edu>
2026-08-10 17:34:02 -0500

    Update to main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -3,6 +3,8 @@
 #include "s5/helpers.hpp"
 #include "s2/dataloader.hpp"
 
+void test ();
+
 int main (int argc, char** argv) {
     std::string config_file = "config.txt";
     Runner r;
@@ -10,8 +12,16 @@ int main (int argc, char** argv) {
 
     // If training mode, the argument is structured as ./app <config>
     if (argc == 2) {
-        config_file = argv[1];
-        r.Run (config_file);
+        std::string arg1 = argv[1];
+        if (arg1 == "--test") {
+            // Test code
+            test ();
+
+        }
+        else {
+            config_file = argv[1];
+            r.Run (config_file);
+        }
     }
     else {
         // Invalid arguments
@@ -21,3 +31,39 @@ int main (int argc, char** argv) {
 
     return 0;
 }       
+
+void test () {
+    Scheduler2 scheduler;
+    scheduler.Start (
+        0,
+        1600,
+        2716,
+        s3_Windowing_Mode::FULLSCREEN,
+        s3_TargetFPS_Mode::NO_TARGET_FPS,
+        30,
+
+        0.0f, // Delay_us
+        10,   // Average_N
+        24,   // Burst_N
+        "192.168.2.10",
+        8000,
+        1600, // PEncoder_Height
+        2716, // PEncoder_Width
+        32,   // Num_Levels
+        PLM_Device_Enum::NIR,
+
+        nullptr // opt
+    );
+
+    scheduler.SetRange(0.5); // Set range to 0.5 for testing
+
+    // Random phase between 0 and 2*pi for (24, 1600, 2716)
+    auto random_phase = torch::rand({24, 1600, 2716}) * 2 * M_PI;
+
+    scheduler.SetTextureFromTensor(random_phase);
+    std::cout << "INFO: [test] Displayed random phase texture with range 0.5.\n";
+
+    while (!WindowShouldClose()) {
+        scheduler.DrawTextureToScreen();
+    }
+}
